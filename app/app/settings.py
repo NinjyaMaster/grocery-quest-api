f4ba8db940ddada470cb03c046c1103b381f150c
Mia Watanabe <mia.noriko.watanabe@gmail.com>
2022-09-17 10:25:11 -0700

    Fixed create .env file in Github Actions, move Django SECRET_KEY to .env file

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=*1%)4*(0_g-ge(i20)qt#rxbs39nb9r=$i+jpaemk(b1%g!wu'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
